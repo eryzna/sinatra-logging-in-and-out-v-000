6fcca7c5de44c225906a73f129525c4aab7da270
Elana Ryznar <eryzna@gmail.com>
2019-09-27 04:57:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user=User.find_by(params[:username])
+    @user=User.find_by(username:params[:username])
     if @user
-      puts "cats"
+      session[:user_id]= @user.id
+      redirect to '/account'
     end
+    redirect to '/error'
 
   end
 
